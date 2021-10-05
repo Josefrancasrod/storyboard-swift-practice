6608dd5b7b8d4d7a038d3dae4b75a6ec74bddbf3
Jose Francisco Castillo Rodriguez <josefrancasrod@gmail.com>
2021-10-04 23:50:08 -0500

    Login Screen - Working with IBOutlet and IBAction

--- Aprendiendo/ViewController.swift
+++ Aprendiendo/ViewController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: - Reference  UI
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButtonAction() {
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        
+        if email == "jose@gmail.com", password == "123"{
+            print("Bienvenido")
+            performSegue(withIdentifier: "home_segue", sender: nil)
+        }else{
+            print("Credenciales incorrectas")
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
