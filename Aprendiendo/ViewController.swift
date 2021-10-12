bfaeccdf7d5f0981ffaebdfa40099db9530b54fe
Jose Francisco Castillo Rodriguez <josefrancasrod@gmail.com>
2021-10-11 23:43:13 -0500

    Login Screen - Working with UserDefaults

--- Aprendiendo/ViewController.swift
+++ Aprendiendo/ViewController.swift
@@ -11,24 +11,41 @@ class ViewController: UIViewController {
     //MARK: - Reference  UI
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailSwitch: UISwitch!
     
     @IBAction func loginButtonAction() {
         let email = emailTextField.text
         let password = passwordTextField.text
         
         if email == "jose@gmail.com", password == "123"{
-            print("Bienvenido")
+            if emailSwitch.isOn {
+                //se guarda valores con llave y valor en los userdefaults
+                storage.set(email, forKey: emailKey)
+            }else{
+                storage.removeObject(forKey: emailKey)
+            }
             performSegue(withIdentifier: "home_segue", sender: nil)
         }else{
             print("Credenciales incorrectas")
         }
     }
     
+    private let emailKey = "email-key"
+    private let storage = UserDefaults.standard
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Se busca un string en los UserDefaults y lo asignamos.
+        if let storedEmail = storage .string(forKey: emailKey) {
+            emailTextField.text = storedEmail
+            emailSwitch.isOn = true
+        }else{
+            
+            emailSwitch.isOn = false
+        }
     }
 
 
